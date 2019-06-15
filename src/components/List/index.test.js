f4432fa998efbfc5a3a6b0a38301fd41a476c40e
Mustafa Dal <you@example.com>
2019-06-16 00:01:05 +0300

    jest tests redux fixes.

--- src/components/List/index.test.js
+++ src/components/List/index.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import List from './index'
+import store from '../../store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<List />, div)
+  ReactDOM.render(
+    <Provider store={store}><List /></Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
