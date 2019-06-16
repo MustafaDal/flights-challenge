1cd724c3991186bb9c772e96cd4e8ef160cf5916
Mustafa Dal <you@example.com>
2019-06-17 00:46:07 +0300

    create test file store fixes.

--- src/components/Create.test.js
+++ src/components/Create.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from '../store'
 import Create from './Create'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Create />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <Create />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/components/List/index.test.js
+++ src/components/List/index.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import List from './index'
 import store from '../../store'
+import List from './index'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
